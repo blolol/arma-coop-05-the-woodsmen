0610ee7d3432bd83adf13185ee4af3b9e9b7badf
Ross Paffett <ross@rosspaffett.com>
2013-06-25 14:05:55 -0400

    Don't place MissionDescription inside a control group

--- client/ui/dialogs/bl_ui_missions_dialog.hpp
+++ client/ui/dialogs/bl_ui_missions_dialog.hpp
@@ -74,75 +74,26 @@ class BL_UI_MissionsDialog {
 	class Objects {};
 
 	class Controls {
-		class RightColumn {
+		class MissionDescription {
 			idc = -1;
 
-			type = BL_UI_CONTROLS_GROUP;
-			style = BL_UI_STYLE_MULTI;
-			shadow = 0;
+			type = BL_UI_STATIC;
+			style = BL_UI_STYLE_LEFT + BL_UI_STYLE_MULTI;
+			moving = false;
 
-			x = 0.5;
-			y = 0.0;
+			text = "";
+			font = BL_UI_TEXT_FONT;
+			sizeEx = BL_UI_TEXT_BODY_SIZE;
+			lineSpacing = 1.0;
 
-			w = 0.5;
-			h = 1.0;
+			colorBackground[] = BL_UI_COLOR_TRANSPARENT;
+			colorText[] = BL_UI_COLOR_TEXT;
 
-			class ScrollBar {
-				color[] = BL_UI_COLOR_SCROLLBAR;
-				colorActive[] = BL_UI_COLOR_SCROLLBAR;
-				colorDisabled[] = BL_UI_COLOR_SCROLLBAR_DISABLED;
+			x = 0.0;
+			y = 0.5;
 
-				thumb = "#(argb,8,8,3)color(1,1,1,1)";
-				arrowEmpty = "#(argb,8,8,3)color(1,1,1,1)";
-				arrowFull = "#(argb,8,8,3)color(1,1,1,1)";
-				border = "#(argb,8,8,3)color(1,1,1,1)";
-			};
-
-			class HScrollBar {
-				height = 0.028;
-
-				color[] = BL_UI_COLOR_SCROLLBAR;
-				colorActive[] = BL_UI_COLOR_SCROLLBAR;
-				colorDisabled[] = BL_UI_COLOR_SCROLLBAR_DISABLED;
-				shadow = 0;
-			};
-
-			class VScrollBar {
-				width = 0.021;
-
-				color[] = BL_UI_COLOR_SCROLLBAR;
-				colorActive[] = BL_UI_COLOR_SCROLLBAR;
-				colorDisabled[] = BL_UI_COLOR_SCROLLBAR_DISABLED;
-				shadow = 0;
-
-				autoScrollSpeed = -1;
-				autoScrollDelay = 0;
-				autoScrollRewind = 0;
-			};
-
-			class Controls {
-				class MissionDescription {
-					idc = -1;
-
-					type = BL_UI_STATIC;
-					style = BL_UI_STYLE_LEFT + BL_UI_STYLE_MULTI;
-					moving = false;
-
-					text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum a nibh in commodo. Donec elit ante, hendrerit at diam at, facilisis hendrerit enim. Proin quis leo eget quam suscipit eleifend. Ut tempor laoreet ligula, nec convallis orci malesuada sed. Pellentesque cursus eros sit amet metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum a nibh in commodo. Donec elit ante, hendrerit at diam at, facilisis hendrerit enim. Proin quis leo eget quam suscipit eleifend. Ut tempor laoreet ligula, nec convallis orci malesuada sed. Pellentesque cursus eros sit amet metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum a nibh in commodo. Donec elit ante, hendrerit at diam at, facilisis hendrerit enim. Proin quis leo eget quam suscipit eleifend. Ut tempor laoreet ligula, nec convallis orci malesuada sed. Pellentesque cursus eros sit amet metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum a nibh in commodo. Donec elit ante, hendrerit at diam at, facilisis hendrerit enim. Proin quis leo eget quam suscipit eleifend. Ut tempor laoreet ligula, nec convallis orci malesuada sed. Pellentesque cursus eros sit amet metus.";
-					font = BL_UI_TEXT_FONT;
-					sizeEx = BL_UI_TEXT_BODY_SIZE;
-					lineSpacing = 1.0;
-
-					colorBackground[] = BL_UI_COLOR_TRANSPARENT;
-					colorText[] = BL_UI_COLOR_TEXT;
-
-					x = 0.5;
-					y = 0.0;
-
-					w = 0.4;
-					h = 3.0;
-				};
-			};
+			w = 1.0;
+			h = 0.5;
 		};
 
 		class CloseButton {
