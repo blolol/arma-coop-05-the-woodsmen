ae1d402c414427e1ed2f67fb8b0f79347fb3643c
Ross Paffett <ross@rosspaffett.com>
2013-06-25 17:50:20 -0400

    Add "Accept Mission" button to missions dialog
    * Extract common button UI code into BL_UI_Button class.

--- client/ui.hpp
+++ client/ui.hpp
@@ -1,2 +1,3 @@
 #include "ui\constants.hpp"
+#include "ui\components\bl_ui_button.hpp"
 #include "ui\dialogs.hpp"
--- client/ui/dialogs/bl_ui_missions_dialog.hpp
+++ client/ui/dialogs/bl_ui_missions_dialog.hpp
@@ -106,42 +106,21 @@ class BL_UI_MissionsDialog {
 			h = 0.4;
 		};
 
-		class CloseButton {
+		class CloseButton : BL_UI_Button {
 			idc = BL_UI_BUTTON_IDC_CANCEL;
 			action = "closeDialog 0";
-
-			type = BL_UI_BUTTON;
-			style = BL_UI_STYLE_CENTER;
-			default = false;
-
-			font = BL_UI_TEXT_FONT;
-			sizeEx = BL_UI_TEXT_BUTTON_SIZE;
 			text = "Close";
-
-			colorText[] = BL_UI_COLOR_TEXT;
-			colorFocused[] = BL_UI_COLOR_TEXT;
-			colorDisabled[] = BL_UI_COLOR_TEXT;
-			colorBackground[] = BL_UI_COLOR_BUTTON_BACKGROUND;
-			colorBackgroundDisabled[] = BL_UI_COLOR_BUTTON_BACKGROUND;
-			colorBackgroundActive[] = BL_UI_COLOR_BUTTON_BACKGROUND;
-			colorShadow[] = BL_UI_COLOR_BUTTON_SHADOW;
-			colorBorder[] = BL_UI_COLOR_BUTTON_BORDER;
-			borderSize = 0;
-
-			offsetX = 0.003;
-			offsetY = 0.003;
-			offsetPressedX = 0.002;
-			offsetPressedY = 0.002;
-
-			soundEnter[] = BL_UI_SOUND_NONE;
-			soundPush[] = BL_UI_SOUND_NONE;
-			soundClick[] = BL_UI_SOUND_NONE;
-			soundEscape[] = BL_UI_SOUND_NONE;
-
 			x = 0.01;
 			y = 1.01;
-			w = 0.15;
-			h = 0.05;
+		};
+
+		class AcceptButton : BL_UI_Button {
+			idc = 6;
+			action = "hint 'Mission accepted!'";
+			text = "Accept Mission";
+			x = 0.73;
+			y = 1.01;
+			w = 0.26;
 		};
 	};
 
